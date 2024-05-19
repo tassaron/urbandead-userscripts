fd2ed4f0c408eae05e7311faf9eee1348c617439
tassaron <rutachigal@gmail.com>
2024-05-18 21:04:59 -0400

    fix my username

---
+++ UD_Map_Skinner.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @author         UDToolbar
+// @name           UD Map Skinner
+// @namespace      Tassaron
+// @version        1.2.1
+// @description    Applies images from the original Firefox UDToolbar to the Urban Dead in-game map.
+// @include        https://www.urbandead.com/map.cgi*
+// @exclude        https://www.urbandead.com/map.cgi?logout
+// @downloadURL https://github.com/tassaron/urbandead-userscripts/raw/main/UD_Map_Skinner.user.js
+// @updateURL https://github.com/tassaron/urbandead-userscripts/raw/main/UD_Map_Skinner.meta.js
+// ==/UserScript==
--- UD_Map_Skinner.user.js
+++ UD_Map_Skinner.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
+// @author         UDToolbar
 // @name           UD Map Skinner
 // @namespace      Tassaron
-// @version        1.2.0
-// @description    Applies images from the original Firefox UDToolbar to the UrbanDead in-game map.
-// @updateURL      https://github.com/tassaron2/urbandead-userscripts/raw/main/UD_Map_Skinner.user.js
+// @version        1.2.1
+// @description    Applies images from the original Firefox UDToolbar to the Urban Dead in-game map.
 // @include        https://www.urbandead.com/map.cgi*
 // @exclude        https://www.urbandead.com/map.cgi?logout
+// @downloadURL https://github.com/tassaron/urbandead-userscripts/raw/main/UD_Map_Skinner.user.js
+// @updateURL https://github.com/tassaron/urbandead-userscripts/raw/main/UD_Map_Skinner.meta.js
 // ==/UserScript==
 
 // images converted using http://uri.rappdaniel.com/
@@ -375,16 +377,14 @@ function showCoords(coordinates) {
 
     if (elem.snapshotLength == 0 || !coordinates) return;
 
-    // Create DSS Map link
-    var url = "http://map.dssrzs.org/location/" + coordinates;
-    var link = document.createElement('a');
-    link.href = url;
-    link.target = 'blank';
+    // Create anchor element for coords
+    // This used to be a hyperlink but the website went offline
+    var anchor = document.createElement('a');
     var coords = coordinates.split('-');
     // x = coords[0], y = coords[1]
-    link.textContent = ' [' + coords[0] + ',' + coords[1] + ']';
+    anchor.textContent = ' [' + coords[0] + ',' + coords[1] + ']';
 
-    elem.snapshotItem(0).appendChild(link);
+    elem.snapshotItem(0).appendChild(anchor);
 }
 
 // Function to add filler type blocks if at the edge of the map
